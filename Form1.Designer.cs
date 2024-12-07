4b4e2b25f902e527ac65060b6f1092dd3e8cf12b
NFS11 <blackandyellowenergy@outlook.com>
2024-12-07 09:26:19 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace CustomMessageApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.executeButton = new System.Windows.Forms.Button();
+            this.errorButton = new System.Windows.Forms.Button();
+            this.infoButton = new System.Windows.Forms.Button();
+            this.criticalButton = new System.Windows.Forms.Button();
+            this.compileButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Location = new System.Drawing.Point(12, 12);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(360, 20);
+            this.titleTextBox.TabIndex = 0;
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Location = new System.Drawing.Point(12, 38);
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.Size = new System.Drawing.Size(360, 20);
+            this.messageTextBox.TabIndex = 1;
+            // 
+            // executeButton
+            // 
+            this.executeButton.Location = new System.Drawing.Point(12, 64);
+            this.executeButton.Name = "executeButton";
+            this.executeButton.Size = new System.Drawing.Size(75, 23);
+            this.executeButton.TabIndex = 2;
+            this.executeButton.Text = "Execute";
+            this.executeButton.UseVisualStyleBackColor = true;
+            this.executeButton.Click += new System.EventHandler(this.executeButton_Click);
+            // 
+            // errorButton
+            // 
+            this.errorButton.Location = new System.Drawing.Point(93, 64);
+            this.errorButton.Name = "errorButton";
+            this.errorButton.Size = new System.Drawing.Size(75, 23);
+            this.errorButton.TabIndex = 3;
+            this.errorButton.Text = "Error";
+            this.errorButton.UseVisualStyleBackColor = true;
+            this.errorButton.Click += new System.EventHandler(this.errorButton_Click);
+            // 
+            // infoButton
+            // 
+            this.infoButton.Location = new System.Drawing.Point(174, 64);
+            this.infoButton.Name = "infoButton";
+            this.infoButton.Size = new System.Drawing.Size(75, 23);
+            this.infoButton.TabIndex = 4;
+            this.infoButton.Text = "Information";
+            this.infoButton.UseVisualStyleBackColor = true;
+            this.infoButton.Click += new System.EventHandler(this.infoButton_Click);
+            // 
+            // criticalButton
+            // 
+            this.criticalButton.Location = new System.Drawing.Point(255, 64);
+            this.criticalButton.Name = "criticalButton";
+            this.criticalButton.Size = new System.Drawing.Size(75, 23);
+            this.criticalButton.TabIndex = 5;
+            this.criticalButton.Text = "Critical";
+            this.criticalButton.UseVisualStyleBackColor = true;
+            this.criticalButton.Click += new System.EventHandler(this.criticalButton_Click);
+            // 
+            // compileButton
+            // 
+            this.compileButton.Location = new System.Drawing.Point(12, 93);
+            this.compileButton.Name = "compileButton";
+            this.compileButton.Size = new System.Drawing.Size(75, 23);
+            this.compileButton.TabIndex = 6;
+            this.compileButton.Text = "Compile";
+            this.compileButton.UseVisualStyleBackColor = true;
+            this.compileButton.Click += new System.EventHandler(this.compileButton_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(384, 131);
+            this.Controls.Add(this.compileButton);
+            this.Controls.Add(this.criticalButton);
+            this.Controls.Add(this.infoButton);
+            this.Controls.Add(this.errorButton);
+            this.Controls.Add(this.executeButton);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.titleTextBox);
+            this.Name = "Form1";
+            this.Text = "Custom Message Box App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.Button executeButton;
+        private System.Windows.Forms.Button errorButton;
+        private System.Windows.Forms.Button infoButton;
+        private System.Windows.Forms.Button criticalButton;
+        private System.Windows.Forms.Button compileButton;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.CodeDom.Compiler;
+using Microsoft.CSharp;
+using Microsoft.Win32;
+
+namespace CustomMessageApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void executeButton_Click(object sender, EventArgs e)
+        {
+            string title = titleTextBox.Text;
+            string message = messageTextBox.Text;
+            MessageBox.Show(message, title, MessageBoxButtons.OK);
+        }
+
+        private void errorButton_Click(object sender, EventArgs e)
+        {
+            string title = titleTextBox.Text;
+            string message = messageTextBox.Text;
+            MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void infoButton_Click(object sender, EventArgs e)
+        {
+            string title = titleTextBox.Text;
+            string message = messageTextBox.Text;
+            MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void criticalButton_Click(object sender, EventArgs e)
+        {
+            string title = titleTextBox.Text;
+            string message = messageTextBox.Text;
+            MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void compileButton_Click(object sender, EventArgs e)
+        {
+            string title = titleTextBox.Text;
+            string message = messageTextBox.Text;
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Executable Files (*.exe)|*.exe",
+                Title = "Save Executable File"
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+                string sourceCode = GenerateSourceCode(title, message);
+
+                CompileExecutable(sourceCode, filePath);
+
+                var result = MessageBox.Show("Would you like this .exe to run on system startup?", "Startup Option", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    SetStartup(filePath);
+                }
+
+                MessageBox.Show("Executable file created successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private string GenerateSourceCode(string title, string message)
+        {
+            return $@"
+using System;
+using System.Windows.Forms;
+
+namespace GeneratedMessageApp
+{{
+    public class Program
+    {{
+        [STAThread]
+        public static void Main()
+        {{
+            MessageBox.Show(""{message}"", ""{title}"", MessageBoxButtons.OK);
+        }}
+    }}
+}}";
+        }
+
+        private void CompileExecutable(string sourceCode, string outputPath)
+        {
+            CSharpCodeProvider provider = new CSharpCodeProvider();
+            CompilerParameters parameters = new CompilerParameters
+            {
+                GenerateExecutable = true,
+                OutputAssembly = outputPath
+            };
+
+            parameters.ReferencedAssemblies.Add("System.dll");
+            parameters.ReferencedAssemblies.Add("System.Windows.Forms.dll");
+
+            CompilerResults results = provider.CompileAssemblyFromSource(parameters, sourceCode);
+
+            if (results.Errors.Count > 0)
+            {
+                string errors = string.Join("\n", results.Errors);
+                MessageBox.Show($"Compilation failed:\n{errors}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void SetStartup(string exePath)
+        {
+            string appName = Path.GetFileNameWithoutExtension(exePath);
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+            key.SetValue(appName, exePath);
+        }
+    }
+}
